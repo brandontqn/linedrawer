04d56f2963029fb8e564f3c51bc7bbb652351f9f
brandontqn <brandonn@sfu.ca>
2016-10-07 12:26:40 -0700

    Add files via upload

--- kit361-c/client.cpp
+++ kit361-c/client.cpp
@@ -43,25 +43,31 @@ void Client::draw_rect(int x1, int y1, int x2, int y2, unsigned int color) {
     }
 }
 
-
-void Client::draw_starburst(int x1, int y1, int x2, int y2, int len, int numlin, unsigned int color) {
-    int theta = 360/numlin;
+void Client::draw_starburst(int x1, int x1, int x2, int y2, int len, int numlin, unsigned int color) {
+    int theta = (M_PI + M_PI)/numlin;
+    int tmp_theta = 0;
     int dx = x2-x1;
-    int two_dx = 2*dx;
-    int two_dy = 2*(y2-y1);
+    int two_dx = dx<<1;
+    int dy = y2-y1;
+    int two_dy = dy<<1;
     int t2 = two_dy-two_dx;
     int err = two_dy-dx;
     int y = y1;
-    drawable->setPixel(x1,y1,color);
-
-    for(int i = x1+1; i<x2/2; i++) {
-        if(err>=0){
-            err += t2;
-            y += 1;
+    int px1 = x1-dx;
+    int py1 = y1-dy;
+    for(int j=0; j<numlin/2; j++) {
+        drawable->setPixel(px1,py1,color);
+        for(int i=px1+1; i<x2; i++) {
+            if(err>=0){
+                err += t2;
+                y += 1;
+            }
+            else{
+                err += two_dy;
+            }
+            drawable->setPixel(i,y,colour);
         }
-        else{
-            err += two_dy;
-        }
-        drawable->setPixel(i,y,colour);
+        tmp_theta += theta;
+        px1 +=
     }
 }
--- kit361-c/client.h
+++ kit361-c/client.h
@@ -8,11 +8,11 @@ class Client : public PageTurner
 public:
     Client(Drawable *drawable);
     void nextPage();
-    void draw_starburst(int cx, int cy, int len, int numlin, unsigned int color);
+    void draw_starburst(int x1, int y1, int x2, int y2, int len, int numlin, unsigned int color);
 
 private:
     Drawable *drawable;
-    void draw_rect(int x1, int y1, int x2, int y2, int len, int numlin, unsigned int color);
+    void draw_rect(int x1, int y1, int x2, int y2, unsigned int color);
 };
 
 #endif // CLIENT_H
